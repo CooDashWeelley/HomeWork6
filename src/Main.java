f36d4479e02953a73af9c9965a823ec96b1b8793
CooDashWeelley <iwan.kudashow@gmail.com>
2025-07-08 19:19:46 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 
 public class Main {
     public static void main(String[] args) {
-        // задача 1
+        // task 1
         for (byte i = 1; i <= 10; i ++) {
             System.out.println(i);
+        }
+        // task 2
+        for (byte a = 10; a >= 1; a --) {
+            System.out.println(a);
         }
     }
 }
