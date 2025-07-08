375a436d5a9298f776f48f7abf5ec0e949c449c3
CooDashWeelley <iwan.kudashow@gmail.com>
2025-07-08 20:13:18 +0300

    task 10

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@
 public class Main {
     public static void main(String[] args) {
         // task 1
-        for (byte i = 1; i <= 10; i ++) {
+        for (byte i = 1; i <= 10; i++) {
             System.out.println(i);
         }
         // task 2
-        for (byte a = 10; a >= 1; a --) {
+        for (byte a = 10; a >= 1; a--) {
             System.out.println(a);
         }
         // task 3
@@ -14,7 +14,7 @@ public class Main {
             System.out.println(b);
         }
         // task 4
-        for (byte c = 10; c >= -10; c --) {
+        for (byte c = 10; c >= -10; c--) {
             System.out.println(c);
         }
         // task 5
@@ -31,17 +31,23 @@ public class Main {
         }
         // task 8
         int investment = 0;
-        for (byte month = 1; month <= 12; month ++) {
+        for (byte month = 1; month <= 12; month++) {
             investment += 29000;
             System.out.println("Месяц " + month + ", сумма накоплений равна " + investment + " рублей.");
         }
         // task 9
         int savings = 0;
         int investment2 = 29000;
-        for (byte month = 1; month <= 12; month ++) {
+        for (byte month = 1; month <= 12; month++) {
             savings = savings + savings / 100 * 12 / 12;
             savings = savings + investment2;
             System.out.println("Месяц " + month + ", сумма накоплений равна " + savings + " рублей.");
+        }
+        // task 10
+        int sum;
+        for (byte i = 1; i <= 10; i++) {
+            sum = 2 * i;
+            System.out.println("2*" + i + "=" + sum);
         }
     }
 }
